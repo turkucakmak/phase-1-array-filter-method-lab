5333409aefcb1b50d61eb5d7b44fc6341bc3884f
turku <turkucakmak@hotmail.com>
2023-06-26 01:51:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(array,string){
+   return array.filter(function (arg){
+        return arg.toLowerCase() === string.toLowerCase();
+
+    })
+}
+
+function fuzzyMatch(drivers,query){
+   return drivers.filter(function (arg){
+        return arg.startsWith(query);
+    })
+}
+
+function matchName(driver,string){
+   return driver.filter(function (arg){
+        return arg.name === string
+
+    })
+}
